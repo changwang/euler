fee70111dbb92316e773b90088bc83d5b38c631d
Chang Wang <changwang83@gmail.com>
2014-12-06 18:16:37 -0800

    add challenge14

---
+++ challenge14/challenge14.go
@@ -0,0 +1,29 @@
+package challenge14
+
+func LongestCollatzSequence(target int64) (int64, int64) {
+	var maxNum, maxLength, i int64
+
+	for i = 1; i < target; i++ {
+		seqSize := collatzSequence(i)
+		if seqSize > maxLength {
+			maxNum = i
+			maxLength = seqSize
+		}
+	}
+	return maxNum, maxLength
+}
+
+func collatzSequence(num int64) int64 {
+	sequence := make([]int64, 0)
+	sequence = append(sequence, num)
+	for num != 1 {
+		if num%2 == 0 {
+			num /= 2
+		} else {
+			num = 3*num + 1
+		}
+		sequence = append(sequence, num)
+	}
+	length := len(sequence)
+	return int64(length)
+}
