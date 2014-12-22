dc9722a9ef53d15568534dbc8e8087578926eede
Chang Wang <changwang83@gmail.com>
2014-12-21 22:33:52 -0800

    add challenge37

---
+++ challenge37/challenge37.go
@@ -0,0 +1,57 @@
+package challenge37
+
+import (
+	"euler/utils"
+	"math"
+	"strconv"
+)
+
+func GetAllTruncatablePrimes() int64 {
+	var sum int64
+	var i int64
+	found := 0
+	for i = 10; ; i++ {
+		if IsTruncatablePrime(i) {
+			found++
+			sum += i
+		}
+		if found == 11 {
+			break
+		}
+	}
+
+	return sum
+}
+
+func IsTruncatablePrime(num int64) bool {
+	if num < 10 {
+		return false
+	}
+
+	if !utils.IsPrime(num) {
+		return false
+	}
+
+	shiftLeft := num
+	shiftRight := num
+	for shiftLeft > 10 && shiftRight > 10 {
+		shiftLeft = shiftToLeft(shiftLeft)
+		shiftRight = shiftToRight(shiftRight)
+		if !utils.IsPrime(shiftLeft) || !utils.IsPrime(shiftRight) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func shiftToRight(num int64) int64 {
+	return num / 10
+}
+
+func shiftToLeft(num int64) int64 {
+	numStr := strconv.FormatInt(num, 10)
+	length := len(numStr)
+	base := int64(math.Pow(10, float64(length-1)))
+	return num - (num/base)*base
+}
